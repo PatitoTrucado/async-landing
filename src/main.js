36d2869be083092a7afa5b80aab21fce522acf1d
PatoVamp <toipiola123@gmail.com>
2023-12-19 17:27:35 -0500

    Cosas del asyn-landing

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ const options = {
 
 async function fetchData(urlApi) {
     const response = await fetch(urlApi, options);
-	const data = await response.json();
+	  const data = await response.json();
     return data;
 }
 
